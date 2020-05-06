750d42d521038b467e6a026bf38d3daa549f4c3d
xeroblood <xeroblood@owntheday.io>
2020-05-05 20:28:49 -0400

    Deploy for stage

---
+++ build.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+exit_with_msg() {
+    echo "-------------------"
+    echo "-------------------"
+    echo "$1"
+    echo " "
+    exit "$2";
+}
+
+echo "-------------------"
+echo "Building Dependencies.."
+echo "-------------------"
+cd ./node_modules/gatsby-plugin-ipfs/
+(yarn && yarn build) || exit_with_msg "Build deps failed!" $?
+cd ../../
+
+echo " "
+echo "-------------------"
+echo "Building Project.."
+echo "-------------------"
+gatsby clean
+gatsby build --prefix-paths || exit_with_msg "Build failed!" $?
+
+echo " "
+echo "-------------------"
+echo "Build Complete!"
+echo "-------------------"
+echo " "
--- deploy.sh
+++ deploy.sh
@@ -1,24 +1,20 @@
 #!/usr/bin/env bash
 
-exit_with_msg() {
-    echo "-------------------"
-    echo "-------------------"
-    echo "$1"
-    echo " "
-    exit "$2";
-}
-
+echo "-------------------"
 echo "Bumping version number.."
+echo "-------------------"
 version update -p
 
-echo "Rebuilding.."
-rm -rf public/
-yarn build || exit_with_msg "Build failed!" $?
-
+echo "-------------------"
 echo "Pushing to Github"
+echo "-------------------"
 git add .
 git commit -m "$1"
 git push
 
+echo " "
+echo "-------------------"
+echo "Deploy Complete!"
 echo "-------------------"
 git status
+echo " "
