9de2d9f6329771e85503dcad8c7009b59524fb20
xeroblood <xeroblood@owntheday.io>
2020-05-05 21:10:14 -0400

    Deploy for IPFS via Fleek

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,24 @@
 #!/usr/bin/env bash
 
+exit_with_msg() {
+    echo "-------------------"
+    echo "-------------------"
+    echo "$1"
+    echo " "
+    exit "$2";
+}
+
 echo "-------------------"
 echo "Bumping version number.."
 echo "-------------------"
-version update -p
+version update -p || exit_with_msg "Version bump failed!" $?
 
 echo "-------------------"
 echo "Pushing to Github"
 echo "-------------------"
-git add .
-git commit -m "$1"
-git push
+git add . || exit_with_msg "Adding files to Git failed!" $?
+git commit -m "$1" || exit_with_msg "Committing files to Git failed!" $?
+git push || exit_with_msg "Pushing files to Github failed!" $?
 
 echo " "
 echo "-------------------"
